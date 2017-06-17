19afe857456994f66f3aaa31edb9b6796ee2290e
Jonas Lundberg <jonasl@catchmecode.com>
2017-06-17 23:25:16 +0200

    PyBytecode interpreter: adding method calls
    Creating code-objects:
    They have instructions (obviously), constants,
    variable names and method argument length - this
    would be created by a compiler.
    
    Code ojects live inside stack frames (callstack)
    that carry the code-object itself,
    the local data stack, names and constants.
    
    Arguments to the function is placed on the local
    stack by the calling frame. When the method returns,
    if any value is left in it's local stack, that value
    (the first found) is returned as the result
    of the method call.
    
    Verification:
    * Still prints 7, but this time via a method call.

--- python/bytecode/bytecode.py
+++ python/bytecode/bytecode.py
@@ -1,39 +1,83 @@
-what_to_execute = {
-  "instructions": [
-    # a = 3
-    # b = 4
-    # print(x+y)
-    ("LOAD_VALUE", 0),
-    ("STORE_NAME", 0),
-    ("LOAD_VALUE", 1),
-    ("STORE_NAME", 1),
-    ("LOAD_NAME", 0),
-    ("LOAD_NAME", 1),
-    ("ADD_TWO_VALUES", None),
-    ("PRINT", None)
-  ],
-  "constants": [3, 4],
-  "names": ["a", "b"]
+method_add = {
+    "code": [
+      # func add(x,y):
+      # return x + y
+      # STORE_NAME 0
+      # STORE_NAME 1
+      # LOAD_NAME 0
+      # LOAD_NAME 1
+      # ADD_TWO_VALUES
+      # RET
+      ("STORE_NAME", 0),
+      ("STORE_NAME", 1),
+      ("LOAD_NAME", 0),
+      ("LOAD_NAME", 1),
+      ("ADD_TWO_VALUES", None),
+      ("RET", None)
+    ],
+    "constants": [],
+    "names": ["x", "y"],
+    "args": 2
  }
 
-stack = []
-environment = {}
+method_main = {
+    "code": [
+        # a = 3
+        # b = 4
+        # print(add(a, b))
+        ("LOAD_VALUE", 0),
+        ("STORE_NAME", 0),
+        ("LOAD_VALUE", 1),
+        ("STORE_NAME", 1),
+        ("LOAD_NAME", 0),
+        ("LOAD_NAME", 1),
+        ("CALL", 2),
+        ("PRINT", None)
+    ],
+    "constants": [3, 4, method_add],
+    "names": ["a", "b"],
+    "args": 0
+}
 
-for step in what_to_execute["instructions"]:
-    instruction, value = step
 
-    if instruction == "LOAD_VALUE":
-        num = what_to_execute["constants"][value]
-        stack.append(num)
-    elif instruction == "LOAD_NAME":
-        var_name = what_to_execute["names"][value]
-        var_value = environment[var_name]
-        stack.append(var_value)
-    elif instruction == "STORE_NAME":
-        var_name = what_to_execute["names"][value]
-        environment[var_name] = stack.pop(0)
-    elif instruction == "ADD_TWO_VALUES":
-        op1, op2 = stack.pop(0), stack.pop(0)
-        stack.append(op1 + op2)
-    elif instruction == "PRINT":
-        print(stack.pop(0))
+class Frame:
+    def __init__(self, code_block):
+        self.code_block = code_block
+        self.stack = []
+        self.environment = {}
+
+    def run(self):
+        for step in self.code_block["code"]:
+            instruction, value = step
+
+            if instruction == "LOAD_VALUE":
+                num = self.code_block["constants"][value]
+                self.stack.append(num)
+            elif instruction == "LOAD_NAME":
+                var_name = self.code_block["names"][value]
+                var_value = self.environment[var_name]
+                self.stack.append(var_value)
+            elif instruction == "STORE_NAME":
+                var_name = self.code_block["names"][value]
+                self.environment[var_name] = self.stack.pop(0)
+            elif instruction == "ADD_TWO_VALUES":
+                op1, op2 = self.stack.pop(0), self.stack.pop(0)
+                self.stack.append(op1 + op2)
+            elif instruction == "PRINT":
+                print(self.stack.pop(0))
+            elif instruction == "CALL":
+                code_block = self.code_block["constants"][value]
+                next_frame = Frame(code_block)
+
+                next_frame.stack = self.stack[-2:]
+                self.stack = self.stack[:-2]
+
+                next_frame.run()
+                if len(next_frame.stack) > 0:
+                    self.stack.append(next_frame.stack[0])
+            elif instruction == "RET":
+                break
+
+
+main_frame = Frame(method_main)
+main_frame.run()
