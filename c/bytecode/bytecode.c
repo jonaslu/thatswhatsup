63f65cfaa84a75073b4d5d76221ef835241525e3
Jonas Lundberg <jonasl@catchmecode.com>
2017-08-02 23:11:43 +0200

    Bytecode: added calling of subroutine
    Converted primitives on the const pool
    to objects (ints and code-blocks)
    
    Added calling a method which expects its arguments
    to be on the stack when called - this is wrong - stuff
    might happen before it's arguments are referenced.
    
    I need to add support for either pushing values
    when calling into the functions const-pool or
    or adding support for variables and their values.
    
    It probably leaks memory like my exam-studies
    but it prints 5!

--- c/bytecode/bytecode.c
+++ c/bytecode/bytecode.c
@@ -1,26 +1,3 @@
-// Stack
-
-/*
-PUSH_CONST 1
-CALL 0
-PUSH_CONST 2
-PUSH_CONST 3
-ADD
-RETURN
-ADD
-PRINT
-*/
-
-/*
-typedef enum {
-  PUSH_CONST,
-  CALL,
-  ADD,
-  RETURN,
-  PRINT
-} OP_CODE;
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -30,34 +7,51 @@ static const char ADD = 3;
 static const char RETURN = 4;
 static const char PRINT = 5;
 
+struct Object;
+
 typedef struct {
   char* code;
-  char* constPool;
+  struct Object** constPool;
+
+  int numberArguments;
 } CodeBlock;
+
+typedef enum {
+  TYPE_INT,
+  TYPE_CODE
+} Type;
+
+typedef struct {
+  Type type;
+
+  union {
+    int value;
+    CodeBlock* codeBlock;
+  };
+} Object;
 
 #define MAX_STACK_SLOTS 256
 #define MAX_STACK_FRAMES 256
 
 typedef struct {
-  char slots[MAX_STACK_SLOTS];
+  Object* slots[MAX_STACK_SLOTS];
   int currentStackSlot;
 
   CodeBlock* codeBlock;
   int ip;
 } StackFrame;
 
-// VM
 typedef struct {
   StackFrame* stackFrames[MAX_STACK_FRAMES];
 
   int currentFrame;
 } VM;
 
-void pushStackValue(StackFrame* currentStackFrame, char value) {
+void pushStackValue(StackFrame* currentStackFrame, Object* value) {
   currentStackFrame->slots[currentStackFrame->currentStackSlot++] = value;
 }
 
-char popStackValue(StackFrame* currentStackFrame) {
+Object* popStackValue(StackFrame* currentStackFrame) {
   return currentStackFrame->slots[--currentStackFrame->currentStackSlot];
 }
 
@@ -67,6 +61,11 @@ StackFrame* getCurrentStackFrame(VM* vm) {
 
 char getNextInstruction(StackFrame* currentStackFrame) {
   return currentStackFrame->codeBlock->code[currentStackFrame->ip++];
+}
+
+Object* getConstantPoolValue(StackFrame* currentStackFrame) {
+  char index = getNextInstruction(currentStackFrame);
+  return currentStackFrame->codeBlock->constPool[index];
 }
 
 StackFrame* createStackFrame(CodeBlock *codeBlock) {
@@ -84,58 +83,135 @@ VM* createVM(StackFrame *baseStackFrame) {
   vm->currentFrame = 0;
 }
 
+void pushStackFrame(VM* vm, StackFrame* nextStackFrame) {
+  vm->stackFrames[++vm->currentFrame] = nextStackFrame;
+}
+
+StackFrame* popStackFrame(VM* vm) {
+  return vm->stackFrames[--vm->currentFrame];
+}
+
 void interpret(VM* vm) {
   StackFrame* currentStackFrame = getCurrentStackFrame(vm);
 
-  int exitLoop = 0;
-  do {
+  for(;;) {
     char opcode = getNextInstruction(currentStackFrame);
 
     if (opcode == PUSH_CONST) {
-      char constant = getNextInstruction(currentStackFrame);
-      pushStackValue(currentStackFrame, constant);
+      Object* constObject = getConstantPoolValue(currentStackFrame);
+
+      pushStackValue(currentStackFrame, constObject);
     }
 
     if (opcode == ADD) {
-      char value1 = popStackValue(currentStackFrame);
-      char value2 = popStackValue(currentStackFrame);
+      Object* value1 = popStackValue(currentStackFrame);
+      Object* value2 = popStackValue(currentStackFrame);
 
-      pushStackValue(currentStackFrame, value1 + value2);
+      if (value1->type == TYPE_INT && value2->type == TYPE_INT) {
+        Object* addedValue = malloc(sizeof(Object));
+
+        addedValue->type = TYPE_INT;
+        addedValue->value = value1->value + value2->value;
+
+        pushStackValue(currentStackFrame, addedValue);
+      }
+      // TODO - error on non-addable types
     }
 
     if (opcode == PRINT) {
-      char argument = popStackValue(currentStackFrame);
-      printf("%d", argument);
+      Object* argument = popStackValue(currentStackFrame);
+      printf("%d", argument->value);
+    }
+
+    if (opcode == CALL) {
+      Object* constObject = getConstantPoolValue(currentStackFrame);
+
+      // TODO Assert that const pool object is of TYPE_CODE
+
+      CodeBlock* subRoutineCodeBlock = constObject->codeBlock;
+      StackFrame* nextStackFrame = createStackFrame(constObject->codeBlock);
+
+      for (int i = 0; i < subRoutineCodeBlock->numberArguments; i++) {
+        Object* subroutineArgument = popStackValue(currentStackFrame);
+        pushStackValue(nextStackFrame, subroutineArgument);
+      }
+
+      pushStackFrame(vm, nextStackFrame);
+      currentStackFrame = nextStackFrame;
     }
 
     if (opcode == RETURN) {
-      return;
+      if (vm->currentFrame <= 0) {
+        free(currentStackFrame);
+        return;
+      }
+
+      StackFrame* previousStackFrame = popStackFrame(vm);
+
+      Object* returnValue = popStackValue(currentStackFrame);
+      pushStackValue(previousStackFrame, returnValue);
+
+      free(currentStackFrame);
+
+      currentStackFrame = previousStackFrame;
     }
-  } while(!exitLoop);
+  }
 }
 
 void closeVM(VM* vm) {
-  while(vm->currentFrame-- > 0) {
-    StackFrame *stackFrame = getCurrentStackFrame(vm);
-
-    free(stackFrame->codeBlock->code);
-    free(stackFrame->codeBlock->constPool);
-    free(stackFrame);
-  }
-
+  // TODO Invoke garbage collection
   free(vm);
 }
 
 int main(void) {
-  CodeBlock* codeBlock = malloc(sizeof(CodeBlock));
+  /*
+    // Stack frame now contains first argument (2)
+    int raj(a) {
+      return a + 2
+    }
 
-  char code[] = { 1, 1, 1, 2, 3, 5, 4 };
-  codeBlock->code = &code;
+    PUSH_CONST 0
+    ADD
+    RETURN
+  */
+  CodeBlock* subCodeBlock = malloc(sizeof(CodeBlock));
 
-  char constPool[] = { 1, 2 };
-  codeBlock->constPool = &constPool;
+  char subCode[] = { 1, 0, 3, 4 };
+  Object subConstPool[] = { { TYPE_INT, 2 } };
 
-  StackFrame *baseStackFrame = createStackFrame(codeBlock);
+  subCodeBlock->numberArguments = 1;
+  subCodeBlock->code = &subCode;
+  subCodeBlock->constPool = malloc(sizeof(Object*));
+  subCodeBlock->constPool[0] = &subConstPool[0];
+
+  /*
+    print(1 + raj(2))
+
+    PUSH_CONST 0
+    CALL 1
+
+    PUSH_CONST 2
+    ADD
+    PRINT
+    RETURN
+  */
+
+  CodeBlock* outerCodeBlock = malloc(sizeof(CodeBlock));
+  char code[] = { 1, 0, 2, 1, 1, 2, 3, 5, 4 };
+  outerCodeBlock->code = &code;
+
+  Object outerConstPool[] = { { TYPE_INT, 2 }, { TYPE_CODE, 0 }, { TYPE_INT, 1} };
+  outerConstPool[1].codeBlock = subCodeBlock;
+
+  outerCodeBlock->constPool = malloc(sizeof(Object*) * 3);
+
+  outerCodeBlock->constPool[0] = &outerConstPool[0];
+  outerCodeBlock->constPool[1] = &outerConstPool[1];
+  outerCodeBlock->constPool[2] = &outerConstPool[2];
+
+  Object* test = outerCodeBlock->constPool[0];
+
+  StackFrame* baseStackFrame = createStackFrame(outerCodeBlock);
   VM* vm = createVM(baseStackFrame);
 
   interpret(vm);
