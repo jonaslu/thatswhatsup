5685e1966aa1802a19c43b9b000b77a220864a67
Jonas Lundberg <jonasl@catchmecode.com>
2018-01-11 21:59:02 +0100

    Schemec: scaffolding for a scheme compiler in go
    I've always said that I shouldn't do both new projects
    and languages at the same time. I've also said
    that I should never say never.
    
    Trying my dirty hands on go building a scheme-compiler
    in x86 assembler - following this tutorial:
    http://scheme2006.cs.uchicago.edu/11-ghuloum.pdf
    
    This contains a test-harness (run-assembly.c) that links
    and runs the output of the compiler (compiled.s).
    
    Test plan:
    * Run ./emit-asm-from-c.sh compile-unit.c to
      produce the compile-unit.s file.
    * Run ./make.sh to produce a binary that links
      against the said compile-unit.s file.
    * Run ./run-assembly and watch it produce the
      number 42.

---
+++ go/schemec/make.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc run-assembly.c compile-unit.s -o run-assembly
---
+++ go/schemec/run-assembly.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "compile-unit.h"
+
+int main(void) {
+  printf("%d", scheme_entry());
+  return 0;
+}
