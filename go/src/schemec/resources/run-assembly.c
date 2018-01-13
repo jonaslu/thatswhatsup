ecd25ce7752955f41762614f7cdaea2a5303c80f
Jonas Lundberg <jonasl@catchmecode.com>
2018-01-13 21:02:40 +0100

    Schemec: Grudgingly modify to use go directory layout
    To play nice with the go toolset, adopt a
    go workspace layout (but I'm not doing the
    whole github.com/
    
    Also: creating a tools folder and putting
    some (possibly) useful stuff like emitting
    asm to llvm and x86 for reference.
    
    Test plan:
    * go test schemec/parser passes all tests.
    * go run compiler.go still says everything
      is ok.

--- go/src/schemec/resources/run-assembly.c
+++ go/src/schemec/resources/run-assembly.c
@@ -2,8 +2,14 @@
 
 extern int scheme_entry();
 
-#define int_bitmask 3
-#define int_tag 0
+#define int_bitmask 0b00000011
+#define int_tag 0b00
+
+#define rest_bitmask 0b11111111
+
+#define char_tag 0b00001111
+#define boolean_tag 0b0011111
+#define empty_list 0b00101111
 
 int main(void) {
   int result = scheme_entry();
---
+++ go/src/schemec/tools/emit-llvm-from-c.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+clang -O3 -emit-llvm -S $1
