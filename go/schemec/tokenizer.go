f19b57422d213dce5e3ed3222a1821b7a4ce559e
Jonas Lundberg <jonasl@catchmecode.com>
2018-01-13 19:21:07 +0100

    Schemec: Adding a tokenizer with tests
    Need to tokenize the incoming input stream
    before converting them into primitives.
    
    Test plan:
    * go test tokenizer.go tokenizer_test.go works.
      My GOPATH does not (yet).

---
+++ go/schemec/tokenizer.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"regexp"
+)
+
+// Identifier, boolean (true or false), #\m <- char or integer
+var re = regexp.MustCompile("[0-9][0-9]*|true|false|\\\\#[^\\s]|[a-zA-Z][a-zA-Z0-9]*|\\(|\\)|;[^\n]*")
+
+func split(program string) []string {
+	return re.FindAllString(program, -1)
+}
---
+++ go/schemec/tokenizer_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"testing"
+)
+
+func testSplit(t *testing.T, program string, matches ...string) {
+	result := split(program)
+
+	for num, match := range matches {
+		if result[num] != match {
+			t.Error(result[num])
+		}
+	}
+}
+
+func TestSplitOnBoolean(t *testing.T) {
+	testSplit(t, "true", "true")
+	testSplit(t, "false", "false")
+	testSplit(t, "true false", "true", "false")
+}
+
+func TestSplitOnList(t *testing.T) {
+	testSplit(t, "(", "(")
+	testSplit(t, ")", ")")
+	testSplit(t, "(   )", "(", ")")
+}
+
+func TestSplitOnChar(t *testing.T) {
+	testSplit(t, "\\#m", "\\#m")
+	testSplit(t, "\\#A", "\\#A")
+	testSplit(t, "\\#0", "\\#0")
+}
+
+func TestSplitOnInteger(t *testing.T) {
+	testSplit(t, "1234", "1234")
+	testSplit(t, "1", "1")
+	testSplit(t, "0", "0")
+}
+
+func TestSplitOnComment(t *testing.T) {
+	testSplit(t, ";;  Whatever\n", ";;  Whatever")
+}
+
+func TestSplitOnAssorted(t *testing.T) {
+	testSplit(t, "false true 0 \\#m", "false", "true", "0", "\\#m")
+	testSplit(t, "(if true ;;  Whatever\n", "(", "if", "true", ";;  Whatever")
+}
