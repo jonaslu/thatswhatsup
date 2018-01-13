2999684254417b07824503c634765e5db2fc2b44
Jonas Lundberg <jonasl@catchmecode.com>
2018-01-13 13:47:05 +0100

    Schemec: Beefing up with some more test-harness and tests
    Redo the testing a bit so it always returns
    a value.
    
    Add some more cases.
    
    Test plan:
    * go run compiler.go now returns different integers
      that all should pass.

--- go/schemec/compiler.go
+++ go/schemec/compiler.go
@@ -9,46 +9,56 @@ import (
 	"strings"
 )
 
-func assert(testcase string, expected string, actual string) {
+func assert(testcase string, expected string, actual string) bool {
 	if expected != actual {
 		fmt.Println("Tescase " + testcase + " failed. Expected " + expected + " actual " + actual)
+
+		return false
 	}
+
+	return true
 }
 
-func compile() string {
+func compile(program string) string {
 	content, err := ioutil.ReadFile("resources/compile-unit.s")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	runcode := strings.Replace(string(content), "[insert]", "movl $42, %eax", 1)
+	runcode := strings.Replace(string(content), "[insert]", "movl $"+program+", %eax", 1)
 
 	return runcode
 }
 
-func writeRunCode(runcode string) {
-	file, err := os.Create("output/compile-unit.s")
+func writeAssemblyFile(runcode string) string {
+	assemblyFilePath := "output/compile-unit.s"
+	file, err := os.Create(assemblyFilePath)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	file.WriteString(runcode)
+
+	return assemblyFilePath
 }
 
-func makeRunCodeBinary() {
-	gccBinaryCmd := exec.Command("gcc", "resources/run-assembly.c", "output/compile-unit.s", "-o", "output/run-assembly")
+func makeRunCodeBinary(assemblyOutputFile string) string {
+	binaryName := "output/run-assembly"
+	gccBinaryCmd := exec.Command("gcc", "resources/run-assembly.c", assemblyOutputFile, "-o", binaryName)
 
 	_, err := gccBinaryCmd.Output()
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	return binaryName
 }
 
-func runTests() {
-	compiledBinaryOutput := exec.Command("output/run-assembly")
+func captureBinaryOutput(binaryFilePath string) string {
+	compiledBinaryOutput := exec.Command(binaryFilePath)
 
 	result, err := compiledBinaryOutput.Output()
 
@@ -56,12 +66,34 @@ func runTests() {
 		log.Fatal(err)
 	}
 
-	assert("Output to equal 42", "42", string(result))
+	return string(result)
+}
+
+func runTest(testname string, program string, output string) bool {
+	assembly := compile(program)
+	assemblyFilePath := writeAssemblyFile(assembly)
+	binaryFilePath := makeRunCodeBinary(assemblyFilePath)
+	result := captureBinaryOutput(binaryFilePath)
+
+	return assert("Output to equal 42", output, result)
+}
+
+func runIntegerTests() {
+	fmt.Println("Running integer tests")
+
+	testsPassed := true
+
+	testsPassed = testsPassed && runTest("Outout to equal 42", "42", "42")
+	testsPassed = testsPassed && runTest("Outout to equal 666", "666", "666")
+	testsPassed = testsPassed && runTest("Outout to equal 0", "0", "0")
+
+	if testsPassed {
+		fmt.Println("OK")
+	} else {
+		fmt.Println("Fail")
+	}
 }
 
 func main() {
-	runcode := compile()
-	writeRunCode(runcode)
-	makeRunCodeBinary()
-	runTests()
+	runIntegerTests()
 }
