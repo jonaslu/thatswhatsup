750d22702dc5231df632cadb79cd14c707139423
Jonas Lundberg <jonasl@catchmecode.com>
2018-01-25 21:30:38 +0100

    Schemec: adding tokenizer and parser
    Ah, tokenizing and parsing - all done
    in the same file. Ye ancient tradition
    of taking a stream of tokens and turning
    them into a list of symbols.
    
    Test plan:
    * Unit tests pass (go test schmec/parser/)

--- go/src/schemec/parser/tokenizer.go
+++ go/src/schemec/parser/tokenizer.go
@@ -1,12 +1,122 @@
 package parser
 
 import (
+	"errors"
+	"log"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
-// integer boolean char identifier or comment
-var re = regexp.MustCompile("0|[-]?[1-9][0-9]*|true|false|\\\\#[^\\s]|[a-zA-Z][a-zA-Z0-9]*|\\(|\\)|;[^\n]*")
+// integer boolean char comment or identifier
+var re = regexp.MustCompile("0|[-]?[1-9][0-9]*|true|false|\\\\#[^\\s]|\\(|\\)|;[^\n]*|[^\\s\\(\\);]+")
 
 func tokenize(program string) []string {
 	return re.FindAllString(program, -1)
+}
+
+type (
+	// Symbol symbol ast value
+	Symbol struct {
+		value string
+	}
+)
+
+func peekToken(tokens []string) (string, []string) {
+	if len(tokens) == 0 {
+		return "", tokens
+	}
+
+	if len(tokens) == 1 {
+		return tokens[0], []string{}
+	}
+
+	return tokens[0], tokens[1:]
+}
+
+func parseList(tokens []string) ([]string, []interface{}, error) {
+	returnList := []interface{}{}
+
+	for len(tokens) > 0 {
+		nextToken, remainingTokens := peekToken(tokens)
+
+		if nextToken == ")" {
+			return remainingTokens, returnList, nil
+		}
+
+		remainingTokens, parseResult, err := parse(tokens)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		returnList = append(returnList, parseResult)
+		tokens = remainingTokens
+	}
+
+	return nil, nil, errors.New("End of list not found")
+}
+
+func parse(tokens []string) ([]string, interface{}, error) {
+	nextToken, remainingTokens := peekToken(tokens)
+
+	if nextToken == "(" {
+		return parseList(remainingTokens)
+	}
+
+	if strings.HasPrefix(nextToken, "\\#") {
+		// A rune - represents a char
+		return remainingTokens, []rune(nextToken)[2:3][0], nil
+	}
+
+	if nextToken == "true" {
+		return remainingTokens, true, nil
+	}
+
+	if nextToken == "false" {
+		return remainingTokens, false, nil
+	}
+
+	intValue, err := strconv.Atoi(nextToken)
+	if err == nil {
+		return remainingTokens, intValue, nil
+	}
+
+	return remainingTokens, Symbol{nextToken}, nil
+}
+
+func filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
+
+// GetAst takes a string and produces an ast
+func GetAst(program string) (interface{}, error) {
+	if program == "" {
+		return nil, nil
+	}
+
+	tokens := tokenize(program)
+	tokens = filter(tokens, func(v string) bool { return !strings.HasPrefix(v, ";") })
+
+	if len(tokens) == 0 {
+		return nil, nil
+	}
+
+	tokens, ast, err := parse(tokens)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(tokens) > 0 {
+		return nil, errors.New("Unrecognized symbol")
+	}
+
+	return ast, nil
 }
--- go/src/schemec/parser/tokenizer_test.go
+++ go/src/schemec/parser/tokenizer_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -46,4 +47,80 @@ func TestTokenizeOnComment(t *testing.T) {
 func TestTokenizeOnAssorted(t *testing.T) {
 	testTokenize(t, "false true 0 \\#m", "false", "true", "0", "\\#m")
 	testTokenize(t, "(if true ;;  Whatever\n", "(", "if", "true", ";;  Whatever")
+}
+
+func testGetAst(t *testing.T, program string, match interface{}) {
+	result, error := GetAst(program)
+
+	if error != nil {
+		if error.Error() != match {
+			t.Error(error)
+		}
+
+		return
+	}
+
+	if result == nil {
+		if match != nil {
+			t.Error(result, match)
+		}
+
+		return
+	}
+
+	if reflect.TypeOf(result).String() == "[]interface {}" {
+		if !reflect.DeepEqual(result, match) {
+			t.Error(result, match)
+		}
+
+		return
+	}
+
+	if result != match {
+		t.Error(result, match)
+	}
+}
+
+func TestGetAstForIntegers(t *testing.T) {
+	testGetAst(t, "1", 1)
+	testGetAst(t, "1234", 1234)
+	testGetAst(t, "-1234", -1234)
+}
+
+func TestGetAstForBooleans(t *testing.T) {
+	testGetAst(t, "true", true)
+	testGetAst(t, "false", false)
+}
+
+func TestGetAstForChars(t *testing.T) {
+	testGetAst(t, "\\#m", rune('m'))
+}
+
+func TestGetAstForLists(t *testing.T) {
+	testGetAst(t, "()", []interface{}{})
+	testGetAst(t, "(1 2 3)", []interface{}{1, 2, 3})
+	testGetAst(t, "(add 2 3)", []interface{}{Symbol{"add"}, 2, 3})
+	testGetAst(t, "(+ 2 3)", []interface{}{Symbol{"+"}, 2, 3})
+	testGetAst(t, "((+ 2 3) (+ 3 a))", []interface{}{[]interface{}{Symbol{"+"}, 2, 3}, []interface{}{Symbol{"+"}, 3, Symbol{"a"}}})
+}
+
+func TestGetAstForComments(t *testing.T) {
+	testGetAst(t, ";; all comments baby", nil)
+	testGetAst(t, "((+ 2 3) (+ 3 a) ;; yahrrr\n)", []interface{}{[]interface{}{Symbol{"+"}, 2, 3}, []interface{}{Symbol{"+"}, 3, Symbol{"a"}}})
+}
+
+func TestGetAstForSymbols(t *testing.T) {
+	testGetAst(t, "a", Symbol{"a"})
+	testGetAst(t, "aab", Symbol{"aab"})
+	testGetAst(t, "AAb", Symbol{"AAb"})
+}
+
+func TestNotAllowedSymbols(t *testing.T) {
+	testGetAst(t, "1aa", "Unrecognized symbol")
+}
+
+func TestGetAstMixedCases(t *testing.T) {
+	testGetAst(t, "", nil)
+	testGetAst(t, "   (+    2 3) ;; comment", []interface{}{Symbol{"+"}, 2, 3})
+	testGetAst(t, "   ((+    2 3) ;; comment", "End of list not found")
 }
