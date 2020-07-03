be8154101f37cdc704464d3f4b1c46f91521171d
Jonas Lundberg <jonasl@iamjonas.me>
2020-07-03 15:55:07 +0200

    Wayland: rename connect to client
    Because it's starting to look like a client

--- c/wayland/client/mak.sh
+++ c/wayland/client/mak.sh
@@ -3,4 +3,4 @@ XDG_SHELL_XML=/usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml
 wayland-scanner client-header $XDG_SHELL_XML xdg-shell-client-protocol.h
 wayland-scanner private-code $XDG_SHELL_XML xdg-shell-protocol.c
 
-gcc -Werror -O2 -std=c99 -pedantic -Wno-unused-variable -lwayland-client -o connect connect.c xdg-shell-protocol.c
+gcc -Werror -O2 -std=c99 -pedantic -Wno-unused-variable -lwayland-client -o client client.c xdg-shell-protocol.c
